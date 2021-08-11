b0394f9c0b27b11223da66054a12856226be1bdd
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2021-08-11 10:02:49 -0400

    initial CRA setup, Tailwind config

---
+++ client-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./common/Header/Header";
+
+import "./styles/output.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
